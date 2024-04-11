feb09a513ff1b968095bd3ac6a5dacae2c5d0350
s1ches <1chessmic@gmail.com>
2024-04-12 02:57:35 +0300

    [lab_2] done

---
+++ laboratory_work2/constants.py
@@ -0,0 +1,5 @@
+data_for_lower_confidence_limit_for_variance_filepath: str = "data/r3z2.csv"
+confidence_probability: float = 0.99
+n = 68
+
+
---
+++ laboratory_work2/data_handlers/DataHandler.py
@@ -0,0 +1,16 @@
+import numpy as np
+from constants import confidence_probability
+from constants import n
+from scipy import stats
+
+
+def get_lower_confidence_limit_for_variance(data: np.ndarray[float]) -> float:
+    data = np.sort(data)
+
+    mean = np.sum(data) / n
+
+    displaced_dispersion: float = np.sum((data - mean)**2) / n
+
+    alpha = confidence_probability
+
+    return n / stats.chi2.ppf(alpha, n - 1) * displaced_dispersion
